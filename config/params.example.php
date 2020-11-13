d482e2d9630ac95d3147090bd4d3542d268e20c5
A Tin <zinzinx8@gmail.com>
2020-11-13 11:20:18 +0700

    Update config

---
+++ config/params.example.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    'access_token' => '3PPewWyp_DjC4aSrnSxWeqvTUOK6-8my',
+    'api_site_url' => trim('http://golbal.iziweb.net', '/'),
+    'api_version' => 'v1'
+];
