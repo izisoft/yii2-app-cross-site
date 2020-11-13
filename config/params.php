6214e265e9a869667ce193b2c7b25ee6ce8472b2
A Tin <zinzinx8@gmail.com>
2020-11-13 09:44:08 +0700

    Update nz1

--- config/params.php
+++ config/params.php
@@ -5,6 +5,6 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'access_token' => 'Iu4Ed7CKsrDr9uGaJRytjR0832x4JOaw',
-    'api_site_url' => trim('http://yii2.local.iziweb.net:8080', '/'),
+    'api_site_url' => trim('http://yii2.local.iziweb.net', '/'),
     'api_version' => 'v1'
 ];
--- themes/nz1/partials/index_s3.php
+++ themes/nz1/partials/index_s3.php
@@ -37,50 +37,52 @@
                                     <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-7d61eff0" data-id=7d61eff0 data-element_type=column>
                                         <div class="elementor-column-wrap elementor-element-populated">
                                             <div class=elementor-widget-wrap>
-<?php                                
+<?php
 $advs = Yii::$app->f->getAdvert('ADV_WHY_US');
 if(!(isset($advs['data']) && !empty($advs['data']))){
     $advs = [
         'name' => 'Why choosse', 
         'code' => 'ADV_WHY_US',
         'data' => [
-            ['name' => 'Lộ trình vững chắc', 'src' => '#', 'link' => '#', 'description' => 'Đào tạo tiếng Nhật - Tư vấn đơn hàng miễn phí - Hoàn thiện thủ tục đi Nhật - Giới thiệu việc làm khi về nước'],
-            ['name' => 'HỌC TIẾNG NHẬT HIỆU QUẢ', 'src' => '#', 'link' => '#', 'description' => 'Giáo trình tối ưu - Học qua video sinh động, dễ hiểu - Ôn tập kiến thức qua game - Thi thử không giới hạn'],
-            ['name' => 'Đơn vị tư vấn xuất khẩu lao động Nhật Bản hàng đầu', 'src' => '#', 'link' => '#', 'description' => 'Mỗi năm đưa hơn 5000 người đi Nhật - 8 văn phòng đại diện tại Nhật để hỗ trợ - 3 trung tâm đạo tạo quy mô  lớn'],
-            ['name' => 'Đơn hàng phong phú, thu nhập cao, Chi phí thấp', 'src' => '#', 'link' => '#', 'description' => 'Đơn hàng liên tục - Công việc ổn định - Lương cơ bản từ 30 - 40 triệu/tháng - Chi phí thấp nhất thị trường'],
+            [
+                'name' => 'Lộ trình vững chắc', 'src' => '#', 'link' => '#', 
+                'description' => 'Đào tạo tiếng Nhật - Tư vấn đơn hàng miễn phí - Hoàn thiện thủ tục đi Nhật - Giới thiệu việc làm khi về nước.',
+                'image' => '/img/Asset-17.png ',
+            ],
+            [
+                'name' => 'HỌC TIẾNG NHẬT HIỆU QUẢ', 'src' => '#', 'link' => '#', 'description' => 'Giáo trình tối ưu - Học qua video sinh động, dễ hiểu - Ôn tập kiến thức qua game - Thi thử không giới hạn.',
+                'image' => '/img/Asset-18.png ',
+            ],
+            [
+                'name' => 'ĐV tư vấn XKLĐ Nhật uy tín', 'src' => '#', 'link' => '#', 'description' => 'Mỗi năm đưa hơn 10,000 người đi Nhật - Trên 20 Trung tâm đào tạo quy mô lớn.',
+                'image' => '/img/Asset-21.png ',
+            ],
+            [
+                'name' => 'Đơn hàng thu nhập cao', 'src' => '#', 'link' => '#', 'description' => 'Đơn hàng liên tục - Công việc ổn định - Lương cơ bản từ 30 - 40 triệu/tháng - Chi phí thấp.',
+                'image' => '/img/Asset-20.png ',
+            ],
         ],
     ];
 }
-?>                                        
-                                                <div class="elementor-element elementor-element-686869b2 elementor-position-left elementor-vertical-align-middle item_why1 elementor-widget elementor-widget-image-box" data-id=686869b2 data-element_type=widget data-widget_type=image-box.default>                                                    
-                                                    <div class=elementor-widget-container>
-                                                        <div class=elementor-image-box-wrapper>
-                                                            <figure class=elementor-image-box-img>
-                                                                <img width=61 height=68 src=https://gojapan.vn/wp-content/uploads/2020/04/Asset-17.png class="attachment-full size-full" alt="Trang chủ" loading=lazy title="Trang chủ 39">
-                                                            </figure>
-                                                            <div class=elementor-image-box-content>
-                                                                <h3 class="elementor-image-box-title">Lộ trình vững chắc</h3>
-                                                                <p class=elementor-image-box-description>Đào tạo tiếng Nhật - Tư vấn đơn hàng miễn phí - Hoàn thiện thủ tục đi Nhật - Giới thiệu việc làm khi về nước</p>
-
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                    
-                                                <div class="elementor-element elementor-element-5ff92a63 elementor-position-left elementor-vertical-align-middle item_why2 elementor-widget elementor-widget-image-box" data-id=5ff92a63 data-element_type=widget data-widget_type=image-box.default>
-                                                    <div class=elementor-widget-container>
-                                                        <div class=elementor-image-box-wrapper>
-                                                            <figure class=elementor-image-box-img>
-                                                                <img width=63 height=63 src=https://gojapan.vn/wp-content/uploads/2020/04/Asset-18.png class="attachment-full size-full" alt="Trang chủ" loading=lazy title="Trang chủ 40">
-                                                            </figure>
-                                                            <div class=elementor-image-box-content>
-                                                                <h3 class="elementor-image-box-title">HỌC TIẾNG NHẬT ONLINE HIỆU QUẢ</h3>
-                                                                <p class=elementor-image-box-description>Giáo trình tối ưu - Học qua video sinh động, dễ hiểu - Ôn tập kiến thức qua game - Thi thử không giới hạn</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-
+if(isset($advs['data']) && !empty($advs['data'])){
+    foreach($advs['data'] as $k => $adv){
+        if($k > 1) break;
+        echo '<div class="elementor-element elementor-element-686869b2 elementor-position-left elementor-vertical-align-middle item_why'.($k+1).' elementor-widget elementor-widget-image-box" data-id="686869b2" data-element_type="widget" data-widget_type="image-box.default">                                                    
+            <div class="elementor-widget-container">
+                <div class="elementor-image-box-wrapper">
+                    <figure class="elementor-image-box-img">
+                        <img width="61" height="68" src="'.$adv['image'].'" class="attachment-full size-full" alt="'.$adv['name'].'" loading="lazy" title="'.$adv['name'].'">
+                    </figure>
+                    <div class="elementor-image-box-content">
+                        <h3 class="elementor-image-box-title">'.$adv['name'].'</h3>
+                        <p class="elementor-image-box-description">'.$adv['description'].'</p>
+                    </div>
+                </div>
+            </div>
+        </div>';
+    }
+}
+?>
                                             </div>
                                         </div>
                                     </div>
@@ -91,7 +93,7 @@ if(!(isset($advs['data']) && !empty($advs['data']))){
                                                 <div class="elementor-element elementor-element-2e97d3f3 elementor-widget elementor-widget-image" data-id=2e97d3f3 data-element_type=widget data-widget_type=image.default>
                                                     <div class=elementor-widget-container>
                                                         <div class=elementor-image>
-                                                            <img width=199 height=199 src=https://gojapan.vn/wp-content/uploads/2020/04/Gojapan-xkld-nhat-ban-2.png class="attachment-full size-full" alt="Trang chủ" loading=lazy srcset="https://gojapan.vn/wp-content/uploads/2020/04/Gojapan-xkld-nhat-ban-2.png 199w, https://gojapan.vn/wp-content/uploads/2020/04/Gojapan-xkld-nhat-ban-2-150x150.png 150w, https://gojapan.vn/wp-content/uploads/2020/04/Gojapan-xkld-nhat-ban-2-75x75.png 75w" sizes="(max-width: 199px) 100vw, 199px" title="Trang chủ 41">
+                                                            <img width=199 height=199 src="/img/bg2.png" class="attachment-full size-full" alt="" loading="lazy" >
                                                         </div>
                                                     </div>
                                                 </div>
@@ -101,33 +103,27 @@ if(!(isset($advs['data']) && !empty($advs['data']))){
                                     <div class="elementor-column elementor-col-33 elementor-inner-column elementor-element elementor-element-7261527c" data-id=7261527c data-element_type=column>
                                         <div class="elementor-column-wrap elementor-element-populated">
                                             <div class=elementor-widget-wrap>
-                                                <div class="elementor-element elementor-element-362355b4 elementor-position-right elementor-vertical-align-middle item_why3 elementor-widget elementor-widget-image-box" data-id=362355b4 data-element_type=widget data-widget_type=image-box.default>
-                                                    <div class=elementor-widget-container>
-                                                        <div class=elementor-image-box-wrapper>
-                                                            <figure class=elementor-image-box-img>
-                                                                <img width=79 height=71 src=https://gojapan.vn/wp-content/uploads/2020/04/Asset-21.png class="attachment-full size-full" alt="Trang chủ" loading=lazy title="Trang chủ 42">
-                                                            </figure>
-                                                            <div class=elementor-image-box-content>
-                                                                <h3 class="elementor-image-box-title">Đơn vị tư vấn xuất khẩu lao động Nhật Bản hàng đầu</h3>
-                                                                <p class=elementor-image-box-description>Mỗi năm đưa hơn 1000 người đi Nhật - 8 văn phòng đại diện tại Nhật để hỗ trợ - 3 trung tâm đạo tạo quy mô  lớn</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                
-                                                <div class="elementor-element elementor-element-23f3993a elementor-position-right elementor-vertical-align-middle item_why4 elementor-widget elementor-widget-image-box" data-id=23f3993a data-element_type=widget data-widget_type=image-box.default>
-                                                    <div class=elementor-widget-container>
-                                                        <div class=elementor-image-box-wrapper>
-                                                            <figure class=elementor-image-box-img>
-                                                                <img width=71 height=59 src=https://gojapan.vn/wp-content/uploads/2020/04/Asset-20.png class="attachment-full size-full" alt="Trang chủ" loading=lazy title="Trang chủ 43">
-                                                            </figure>
-                                                            <div class=elementor-image-box-content>
-                                                                <h3 class="elementor-image-box-title">Đơn hàng phong phú, thu nhập cao, Chi phí thấp</h3>
-                                                                <p class=elementor-image-box-description>Đơn hàng liên tục - Công việc ổn định - Lương cơ bản từ 30 - 40 triệu/tháng - Chi phí thấp nhất thị trường</p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
+<?php
+if(isset($advs['data']) && !empty($advs['data'])){
+    foreach($advs['data'] as $k => $adv){
+        if($k < 2) continue;
+        echo '<div class="elementor-element elementor-element-686869b2 elementor-position-left elementor-vertical-align-middle item_why'.($k+1).' elementor-widget elementor-widget-image-box" data-id="686869b2" data-element_type="widget" data-widget_type="image-box.default">                                                    
+            <div class="elementor-widget-container">
+                <div class="elementor-image-box-wrapper">
+                    <figure class="elementor-image-box-img">
+                        <img width="61" height="68" src="'.$adv['image'].'" class="attachment-full size-full" alt="'.$adv['name'].'" loading="lazy" title="'.$adv['name'].'">
+                    </figure>
+                    <div class="elementor-image-box-content">
+                        <h3 class="elementor-image-box-title">'.$adv['name'].'</h3>
+                        <p class="elementor-image-box-description">'.$adv['description'].'</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        ';
+    }
+}
+?>
                                             </div>
                                         </div>
                                     </div>
