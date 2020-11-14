464a179cf0891ef7210ce9f7aad1fc6c8c38dd6c
A Tin <zinzinx8@gmail.com>
2020-11-14 14:21:44 +0700

    Update Source 14/11

---
+++ controllers/TextController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class TextController extends Controller
+{
+     
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+
+    public function actionDetail()
+    {
+        return $this->render('detail');
+    }
+
+     
+}
